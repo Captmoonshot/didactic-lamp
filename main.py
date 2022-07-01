436e5e5e3810db6c073aae65d58460343d3e0d75
Captmoonshot <gygantor@protonmail.com>
2022-07-01 00:21:46 -0700

    updated

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("player.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 # Game Loop
 running = True
@@ -32,7 +33,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # if keystroke is pressed check whether it's right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
 
 
